47ace86bd776afae735d85a64a2d750272def914
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-06-07 12:31:11 +0300

    Small fixes and additions

--- text/chapter3/code/EvalCode.java
+++ text/chapter3/code/EvalCode.java
@@ -1,4 +1,6 @@
-final Object trace = new Object[M + 1];
+final Object trace = new Object[M + 2];
+
+final int[] time = new time[] { 0 };
 
 final VarType callVariable11 = ...;
 
@@ -14,18 +16,22 @@ final VarType callVariableM2 = ...;
 final VarType callVariableM3 = ...;
 
 Object streamResult = souce.stream().
+        .peek(x -> time[0]++)
         .callBefore11(/* args */)
     .callNumber1()
+        .peek(x -> time[0]++)
         .callArfter11(/* args */)
     /* calls from 2 to k - 1 */
         .callBeforeK1(/* args */)
         .callBeforeK2(/* args */)
     .callNumberK()
+        .peek(x -> time[0]++)
         .callAfterK1(/* args */)
         .callAfterK2(/* args */)
     /* calls from k + 1 to M - 1  */ 
         .callBeforeM1(/* args */)
     .callNumberM()
+        .peek(x -> time[0]++)
         .callArfterM1(/* args */)
         .callArfterM2(/* args */)
     .terminal();
